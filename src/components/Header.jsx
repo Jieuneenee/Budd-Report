aa9531ebef522fb50759289422e8d53832bfc584
Parkjieun <0224h@naver.com>
2024-10-12 03:22:31 +0900

    fix: 스크롤 이슈 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,6 @@ const GlobalStyle = createGlobalStyle`
 
   html, body {
     height: 100%;
-    overflow: hidden; // 원하지 않는 스크롤바가 생기지 않도록 설정
   }
 `;
 
@@ -28,9 +27,9 @@ const HeaderDiv = styled.div`
   justify-content: center;
 
   img {
-    width: 40px; // 이미지 크기
+    width: 40px;
     height: auto;
-    margin-right: 10px; // 텍스트와 이미지 사이의 간격
+    margin-right: 10px;
   }
 
   @media (max-width: 768px) {
