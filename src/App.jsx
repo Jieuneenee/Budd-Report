5178476cd85d332054882409c4ac5a8ab0c30214
Parkjieun <0224h@naver.com>
2024-10-13 00:40:50 +0900

    테스트 페이지 포함

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Report from "./pages/Report"; // Report 컴포넌트를 임포트합니다.
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/report/1" />} />
         <Route path="/report/:userId" element={<Report />} />{" "}
       </Routes>
     </Router>
